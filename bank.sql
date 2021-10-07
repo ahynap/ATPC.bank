f4284320d7899ea37440c4eabfeea27e295cd36c
AnchiraP <anchira.ps@mail.kmutt.ac.th>
2021-10-07 23:50:08 +0700

    SQL add account

--- bank.sql
+++ bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2021 at 04:24 PM
+-- Generation Time: Oct 07, 2021 at 06:43 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -28,13 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account` (
-  `AccountID` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `AccountID` int(20) NOT NULL,
   `BrunchName` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `AccountNo` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `AccountNoInfoID` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `UserName` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `Password` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`AccountID`, `BrunchName`, `AccountNo`, `AccountNoInfoID`, `UserName`, `Password`) VALUES
+(1, 'KMUTT', '145879521456', '111', 'sompongsu', 'sususompong'),
+(2, 'BKK', '214517845621', '222', 'PPkerati', 'ppsuza');
 
 -- --------------------------------------------------------
 
@@ -43,12 +51,44 @@ CREATE TABLE `account` (
 --
 
 CREATE TABLE `accountnoinfo` (
-  `AccountNoInfoID` int(20) NOT NULL,
+  `accountnoinfoID` int(20) NOT NULL,
   `Balance` float NOT NULL,
   `AccountNo` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `SerialNo` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `AccountType` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`AccountID`);
+
+--
+-- Indexes for table `accountnoinfo`
+--
+ALTER TABLE `accountnoinfo`
+  ADD PRIMARY KEY (`accountnoinfoID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `AccountID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `accountnoinfo`
+--
+ALTER TABLE `accountnoinfo`
+  MODIFY `accountnoinfoID` int(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
