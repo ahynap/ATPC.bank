70352d5cf0005993caa49ddb052226ae1dee1c38
ahynap <chatchanok.v@mail.kmutt.ac.th>
2021-11-08 19:42:41 +0700

    UI_cleaning_for_ConnectAccount

--- connect_account/connect_account_home.php
+++ connect_account/connect_account_home.php
@@ -1,40 +1,33 @@
 <?php
-
-     /* Connect Database */
-
+    /* Connect Database */
     session_start();
     include('..\server\server.php'); 
 
-     /* Use Email Session to Access Data from Database for Display 'Username' */ 
-
+    /* Use Email Session to Access Data from Database for Display 'Username' */ 
     $Email = $_SESSION['Email'];
     $result = mysqli_query($conn,"SELECT * FROM account WHERE Email = '$Email'");
     $result2 = mysqli_query($conn,"SELECT * FROM account WHERE Email = '$Email'");
     $result3 = mysqli_query($conn,"SELECT * FROM account WHERE Email = '$Email'");
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connect Account</title>
-    <link rel="stylesheet" href="connect_account1.css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Connect Account</title>
+        <link rel="stylesheet" href="Acc_connect.css">
 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href='https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin' rel='stylesheet' type='text/css'>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href='https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin' rel='stylesheet' type='text/css'>
+    </head>
 
-    
-</head>
-
-<body>
-
-
-     <form action="..\connect_account\connect_account_insert_home.php" method="post">
-
-         <!-- ----- Error Alert ----- --> 
-
+    <body>
+        
+        <form action="..\connect_account\connect_account_insert_home.php" method="post">
+        
+        <!-- ----- Error Alert ----- --> 
         <?php if (isset($_SESSION['error'])) : ?>
             <div class="error">
                 <h3>
@@ -46,73 +39,65 @@
             </div>
         <?php endif ?>
 
+        <!-- ----- Niv Bar ----- --> 
+        <div class="niv_bar">
 
-    <!-- ----- logo ATPC bank ----- --> 
+            <!-- ----- logo ATPC bank ----- --> 
+            <div class="logo">
+                <?php while($row3 = mysqli_fetch_array($result3)) { ?>
+                    <input type="image" src="logo_blue.png" width="100" onClick="this.form.action='../MainAccount/mainAccount.php'; submit()" value=<?php echo $row3["Email"]; ?> />  <?php }
+                ?>
+            </div>
 
-    <div class="logo">
-
-         <?php
-             while($row3 = mysqli_fetch_array($result3)) {
-        ?>
-
-        <input type="image" src="logo_blue.png" width="127" onClick="this.form.action='../MainAccount/mainAccount.php'; submit()" value=<?php echo $row3["Email"]; ?> />  
-
-         <?php
-            }
-        ?>
-    </div>
-
-    <!-- ----- User's name show here! ----- -->
-
-        <?php
-             while($row = mysqli_fetch_array($result)) {
-        ?>
-                   <span class="show_name" href="#" style="width: 350px; left: 450px; text-align: center"><?php echo $row["Email"]; ?></span>
-
-        <?php
-            }
-        ?>
-  
-    <img src="client_icon.png" width=58.36 class="icon"> 
-    
-    <!-- ----- logout button ----- --> 
-    <button type="submit" class="btn" onClick="this.form.action='../login_client/login_client.php'; submit()"> LOG OUT </button>
+            <!-- ----- logout button ----- --> 
+            <button type="submit" class="btn" onClick="this.form.action='../login_client/login_client.php'; submit()"> LOG OUT </button>
         
-    <!-- ----- header text ----- --> 
-    <span class="header_txt"> CONNECT TO <br> BANK ACCOUNT </span>
-    
-    <!-- ----- fill information ----- --> 
-    <span class="text_fill"> please fill your information </span>
+            <div class="icon">
+                <img src="client_icon.png" width=58.36> 
+            </div>
 
-    <div class="fill_information">
-        <input type="text" placeholder="Account Number" name="AccountNo"> <br>
-        <input type="text" placeholder="Depositor Name" name="DepositorName"><br>
-        <input type="text" placeholder="Depositor Surname" name="DepositorSurName"><br>
-        <input type="text" placeholder="Branch Name" name="BranchName"><br>
-        <input type="text" placeholder="Serial Number" name="SerialNo"><br>
+            <!-- ----- User's name show here! ----- -->
+            <div class="user_detail">
+                <?php while($row = mysqli_fetch_array($result)) { ?>
+                    <span class="show_name" href="#" style="width: 350px; left: 450px; text-align: center"><?php echo $row["Email"]; ?></span> <?php }
+                ?>
+            </div>
 
-    <input type="text" placeholder="Email number" name="Email" /> 
+        </div>
+        
+            
+        <!-- ----- header text ----- --> 
+        <span class="header_txt"> CONNECT TO <br> BANK ACCOUNT </span>
 
+        <!-- ----- fill information ----- --> 
+        <div class="container_fill_info">
+            
+            <span class="text_fill"> please fill your information </span>
 
-        <select name="AccountType">
-            <option value="" disabled selected>Type of Account</option>
-            <option value="Savings">Savings</option>
-            <option value="Fixed Deposites">Fixed Deposites</option>
-        </select>
-    </div>
+            <div class="fill_information">
+                <input type="text" placeholder="Account Number" name="AccountNo"> <br>
+                <input type="text" placeholder="Depositor Name" name="DepositorName"><br>
+                <input type="text" placeholder="Depositor Surname" name="DepositorSurName"><br>
+                <input type="text" placeholder="Branch Name" name="BranchName"><br>
+                <input type="text" placeholder="Serial Number" name="SerialNo"><br>
+                <input type="text" placeholder="Email number" name="Email" /> <br>
+                
+                <select name="AccountType">
+                    <option value="" disabled selected>Type of Account</option>
+                    <option value="Savings">Savings</option>
+                    <option value="Fixed Deposites">Fixed Deposites</option>
+                </select>
+            </div>
 
-    <!-- ----- Connect Account to ATPC Bank  ----- --> 
-    
-    <div class="connect">
-    <button type="submit" class="btn" name="connect_user"> CONNECT </button>
-    </div>
+            <!-- ----- Connect Account to ATPC Bank  ----- --> 
+            <div class="connect">
+            <button type="submit" class="btn" name="connect_user"> CONNECT </button>
+            </div>
+        </div>
 
-
-
-</body>
-
-
+    </body>
 </html>
+
 <?php
     mysqli_close($conn); 
 ?>
