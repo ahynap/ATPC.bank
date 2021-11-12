5c25c08f8069a5046abf920d30dc2149fe6a5443
ahynap <chatchanok.v@mail.kmutt.ac.th>
2021-11-12 22:15:23 +0700

    UI_cleaning_ModeStaff

--- mode_staff/mode_staff.php
+++ mode_staff/mode_staff.php
@@ -1,12 +1,9 @@
 <?php
-
     /* Connect Database */
-
     session_start();
     include('..\server\server.php'); 
 
     $Email = $_SESSION['Email'];
-
 ?>
 
 <!DOCTYPE html>
@@ -15,40 +12,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mode staff</title>
-    <link rel="stylesheet" href="role.css">
+    <title>Staff</title>
+    <link rel="stylesheet" href="staff_main.css">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href='https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
- 
-    <!-- ----- Login form ----- --> 
+    <!-- ----- Niv Bar ----- --> 
+    <div class="niv_bar">
+
+        <!-- ----- logo ATPC bank ----- --> 
+        <div class="logo">
+            <input type="image" src="logo_blue.png" width="100"/>  
+        </div>
+
+        <!-- ----- logout button ----- --> 
+        <button type="submit" class="btn" onClick="this.form.action='../login_staff/login_staff.php'; submit()"> LOGOUT </button>
+
+        <div class="icon">
+            <img src="staff_icon.png" width=58.36> 
+        </div>
+    </div>
+
+    <!-- ----- header text ----- --> 
+    <span class="header_txt"> WELCOME STAFF !</span>
+    <span class="sub_header"> please choose the option </span>
+
+    <!-- ----- select option ----- --> 
     <form method="post">
+        <div class="function_btn ">
 
-        <div class="back">
-            <button type="submit" class="btn" onClick="this.form.action='../login_staff/login_staff.php'; submit()"> LOGOUT </button>
+            <!-- ----- REGISTER BANK ACCOUNT ----- --> 
+            <div class="each_function">
+                <div class="add_account">
+                    <button type="button" class="btn" onClick="this.form.action='../add_accountNo_staff/add_accountNo_staff.php'; submit()">
+                        REGISTER <br> BANK'S ACCOUNT
+                    </button>
+                </div>
+            </div>
+
+            <!-- ----- VIEW TRANSACTION REPORT  ----- --> 
+            <div class="each_function">
+                <div class="view_report">
+                    <button type="button" class="btn" onClick="this.form.action='../report/report_access.php'; submit()">
+                        VIEW TRANSACTION REPORT   
+                    </button>
+                </div>
+            </div>
+
         </div>
-      
-        <!-- ----- STAFF OPTIONS ----- --> 
-        <span class="Text_question"> STAFF OPTIONS </span>
-
-
-        <!-- ----- Add account number botton ----- --> 
-        <div class="client_role">
-            <button type="button" class="btn" onClick="this.form.action='../add_accountNo_staff/add_accountNo_staff.php'; submit()">
-                Add account number 
-            </button>
-        </div>
-
-        <!-- ----- History botton ----- --> 
-        <div class="staff_role">
-            <button type="button" class="btn" onClick="this.form.action='../report/report_access.php'; submit()">
-                 History   
-            </button>
-        </div>
-
     </form>
    
 </body>
