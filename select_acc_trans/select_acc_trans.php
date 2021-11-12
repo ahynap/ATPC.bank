11b1d56d9327aa32b3349128502530510afb119a
ahynap <chatchanok.v@mail.kmutt.ac.th>
2021-11-12 16:04:42 +0700

    UI_cleaning_for_transfer&select_acc_trans

--- select_acc_trans/select_acc_trans.php
+++ select_acc_trans/select_acc_trans.php
@@ -1,12 +1,10 @@
 <?php
-    
-   /* Connect Database */ 
-
+  
+    /* Connect Database */ 
     session_start();
     include('..\server\server.php');
 
-     /* Use Email Session to Access Data from Database for Display 'Username' , Specify Account Money Type and Verify Account Money Number*/ 
-
+    /* Use Email Session to Access Data from Database for Display 'Username' , Specify Account Money Type and Verify Account Money Number*/ 
     $Email = $_SESSION['Email'];
 
     $result3 = mysqli_query($conn,"SELECT * FROM account WHERE Email = '$Email'");
@@ -29,137 +27,110 @@
     $getResult4 = mysqli_fetch_assoc($result4);
     
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> select account </title>
-    <link rel="stylesheet" href="select_acc_trans.css">
+    <link rel="stylesheet" href="select_acc_transfer.css">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href='https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin' rel='stylesheet' type='text/css'>
-
+    
     <!-- ----- select accout ----- --> 
-    <link rel="stylesheet" href="show_account.css">
-</head>
+    <link rel="stylesheet" href="show_acc.css">
+  </head>
 
-<body>
+  <body>
+    <!-- ----- Niv Bar ----- --> 
+    <div class="niv_bar">
     
-    <!-- ----- logo ATPC bank ----- --> 
-    <div class="logo">
-        <?php
-                    while($row3 = mysqli_fetch_array($result3)) {
-                  ?>
+      <!-- ----- logo ATPC bank ----- --> 
+      <div class="logo">
+          <?php while($row3 = mysqli_fetch_array($result3)) {?>
+            <input type="image" src="logo_blue.png" width=127 onclick="window.location.href='../MainAccount/mainAccount.php'" name="Email" value=<?php echo $row3["Email"]; ?> />  
+          <?php } ?>
+      </div>
 
-                        <input type="image" src="logo_blue.png" width=127 onclick="window.location.href='../MainAccount/mainAccount.php'" name="Email" value=<?php echo $row3["Email"]; ?> />  
+      <!-- ----- logout button ----- --> 
+      <button type="submit" class="btn" onclick="window.location.href='../login_client/login_client.php'"> LOG OUT </button>
 
-                 <?php
-                    }
-                  ?>
-    </div>
+      <div class="icon">
+        <img src="client_icon.png" width=58.36> 
+      </div>
 
-    <!-- ----- user's name show here! ----- --> 
-    
-        <?php
-             while($row5 = mysqli_fetch_array($result5)) {
-        ?>
-                   <span class="show_name" href="#" style="width: 350px; left: 450px; text-align: center"><?php echo $row5["Email"]; ?></span>
+      <!-- ----- user's name show here! ----- --> 
+      <div class="user_detail">
+        <?php while($row5 = mysqli_fetch_array($result5)) { ?>
+          <span class="show_name" href="#" style="width: 350px; left: 450px; text-align: center"><?php echo $row5["Email"]; ?></span>
+        <?php } ?>
+      </div>
 
-        <?php
-            }
-        ?>
-
-    <img src="client_icon.png" width=58.36 class="icon"> 
-    
-    <!-- ----- logout button ----- --> 
-    <button type="submit" class="btn" onclick="window.location.href='../login_client/login_client.php'"> LOG OUT </button>
-
-    <!-- ----- BACK ----- --> 
-     <div class="back">
+      <!-- ----- BACK ----- --> 
+      <div class="back">
         <button type="submit" class="btn"  onclick="window.location.href='../transfer/transfer.php'" > BACK </button>
+      </div>
+
     </div>
+    
+
 
     <!-- ----- header text ----- --> 
-    <span class="header_txt"> SELECT ACCOUNT </span>
+    <span class="header_txt"> SELECT <br> ACCOUNT </span>
     
+    <!-- ----- show account list ----- --> 
+    <?php mysqli_multi_query($conn, $result2);
+      do {
+        if ($result4 = mysqli_store_result($conn)) {
+          $i = 1;
+        while ($row4 = mysqli_fetch_row($result4)) {
+    ?>
 
-    <div class="container_blue">
-        <?php
-             mysqli_multi_query($conn, $result2);
-         do {
+    <!-- ----- ##### now selected accout ##### ----- --> 
+    <form action="..\select_acc_trans\select_acc_trans_insert.php" method="post">
+      <button type="submit" class="select_accout" name="AccountNo" value=<?php echo $row4[0]; ?>>
 
-             if ($result4 = mysqli_store_result($conn)) {
-                 $i = 1;
-              while ($row4 = mysqli_fetch_row($result4)) {
-
-         ?>
-
-        <!-- ----- ##### now selected accout ##### ----- --> 
-          <form action="..\select_acc_trans\select_acc_trans_insert.php" method="post">
-            <button type="submit" class="next_select_accout" name="AccountNo" value=<?php echo $row4[0]; ?>>
-
-            <!-- ----- in blue squre box detail ----- --> 
-
-            <!-- ----- account_type ----- --> 
+        <!-- ----- in blue squre box detail ----- --> 
+          <!-- ----- account_type ----- --> 
             <div class="account_type">
-                  <label> <?php echo $row4[5]; ?></label>
+              <label> <?php echo $row4[5]; ?></label>
             </div>
 
-            <!-- ----- account_number ----- --> 
+          <!-- ----- account_number ----- --> 
             <div class="account_number">
-                   <label> <?php echo $row4[0]; ?></label>
+              <label> <?php echo $row4[0]; ?></label>
             </div>
 
-            <!-- ----- account_number ----- --> 
-             <div class="balance">
-                        <label><?php echo $row4[4]; ?></label>
-                    </div>
-            
-
+          <!-- ----- balance ----- --> 
+            <div class="balance">
+              <label><?php echo $row4[4]; ?></label>
+            </div>
+                
             <!-- ----- if this account is MAIN ACCOUNT please show this section ----- --> 
-             
-                    <?php
-                      if($getResult4['MainAccount'] == $row4[10]) {
-                    ?>
+              <?php if($getResult4['MainAccount'] == $row4[10]) { ?>
+              <div class="main_account">
+                <label>MAIN ACCOUNT</label>
+              </div> <?php } ?>
 
-                    <div class="main_account">
-                        <label>MAIN ACCOUNT</label>
-                    </div> 
-
-                     <?php
-                    }
-                      ?>
-        </button>
-        <br><br><br>
-
-
-        <?php
-
-}
-
-   $i++;
-        }
-
+      </button>
+            
+      <br><br><br>
+            
+      <?php } $i++; }
       if (mysqli_more_results($conn)) {
         printf("-----------------\n");
-  
-    }
-} while (mysqli_next_result($conn));
+      }
+      } while (mysqli_next_result($conn));
+      ?>
 
-  ?>
-
- </form>
-    </div>
-
-    </div>
-
-</body>
+    </form>
 
 
+  </body>
 </html>
-
 
 <?php
     mysqli_close($conn); 
--- transfer/transfer2.php
+++ transfer/transfer2.php
@@ -47,13 +47,13 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>TRANSFER</title>
-      <link rel="stylesheet" href="transferMoney.css">
+      <link rel="stylesheet" href="transfer_money.css">
 
       <link rel="preconnect" href="https://fonts.gstatic.com">
       <link href='https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin' rel='stylesheet' type='text/css'>
 
       <!-- ----- select accout ----- --> 
-      <link rel="stylesheet" href="account_blue_box.css">
+      <link rel="stylesheet" href="show_acc.css">
   </head>
 
   <body>
