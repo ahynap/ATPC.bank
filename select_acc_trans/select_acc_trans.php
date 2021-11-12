f157a5fa8aebc3ca0602676ab7f72cb8c5737030
ahynap <chatchanok.v@mail.kmutt.ac.th>
2021-11-12 19:54:13 +0700

    UI_cleaning_for_select_acc_trans

--- select_acc_trans/select_acc_trans.php
+++ select_acc_trans/select_acc_trans.php
@@ -35,7 +35,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> select account </title>
-    <link rel="stylesheet" href="select_acc_transfer.css">
+    <link rel="stylesheet" href="select_acc.css">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href='https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin' rel='stylesheet' type='text/css'>
